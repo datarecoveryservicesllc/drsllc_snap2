70b961163a2ecb90bb3a02cf4f13a7131aef4c17
datarecoveryservicesllc <datarecoveryservicesllc@gmail.com>
2025-02-25 17:51:30 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
   
   // Redirect to the main index page after a delay (e.g., 3 seconds)
   setTimeout(function() {
-    window.location.href = "/accounts.html"; // Redirect to your main index page
+    window.location.href = "accounts.html"; // Redirect to your main index page
 }, 2400); // 3000ms = 3 seconds
 
 
